57e3398f3f7bcd054df124ae86d7777c7025b0bd
TeufelSL <sebastian.larisch@teufel.de>
2022-06-13 20:38:49 +0200

    add facade

--- src/LayerPlanner/LayerPlannerFacade.php
+++ src/LayerPlanner/LayerPlannerFacade.php
@@ -4,7 +4,22 @@ declare(strict_types=1);
 
 namespace App\LayerPlanner;
 
-final class LayerPlannerFacade
-{
+use App\LayerPlanner\Models\LayerPlan;
+use App\LayerPlanner\Models\LayerPlanInput;
 
+final class LayerPlannerFacade implements LayerPlannerFacadeInterface
+{
+    private LayerPlannerFactory $factory;
+
+    public function __construct(LayerPlannerFactory $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createPlan(LayerPlanInput $layerInput): LayerPlan {
+
+        return $this->factory
+            ->createLayerPlannerByType($layerInput->getLayingType())
+            ->createPlan($layerInput);
+    }
 }
